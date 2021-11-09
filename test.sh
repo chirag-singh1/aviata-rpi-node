85acd278aa231dd29a9ec657ce568014c63e2418
Chirag Singh <66097224+chirag-singh1@users.noreply.github.com>
2021-11-09 13:35:39 -0800

    Delete test.sh

--- test.sh
+++
@@ -1,9 +0,0 @@
-echo 'Executing central node script'
-echo 'Building central node'
-make && \
-chmod +x execute_ground && \
-echo 'Executing central node program' && \
-./execute_ground
-echo 'Central node program complete, cleaning up'
-make clean
-echo 'Done!'
